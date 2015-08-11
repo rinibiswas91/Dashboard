8b4d5602c1f5ac1774cd8fbf248b74e0a5166ae1
rinibiswas91 <biswas.rinee@gmail.com>
2015-08-11 12:16:05 +0530

    add

---
+++ images/lead.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+	main(){
+
+	
+
+	int a[5]={4,2,4,4,5};
+	int b[5]={1,3,2,4,5};
+	int i; int result=1;
+	for(i=1;i<=b[i];i++)
+	{
+	    result*=a[i];
+	    int a= result/10;
+            printf("answer= %d" , a);
+
+	}
+	
+
+}
